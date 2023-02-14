fb232f9e490846cd971e848e01335ade386b3190
shaileshkachare <shaileshkachare2@gmail.com>
2023-02-14 10:16:38 +0530

    Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -26,14 +26,50 @@ const data = {
     'Adventures of Shaktiman'
   ]
 }
-const App = () => {
+// const App = () => {
 
-  return (
-    <div id="main">
-      
+//   return (
+//     <div id="main">
+//       <select>
+//         {data.map(year=>
+//         <option>
+
+//         </option>
+//           )}
+//       </select>
+//     </div>
+//   )
+// }
+
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      selectedYear:null
+    }
+  }
+  changeYear =() =>{
+    this .setState({selectedYear: e.target.value})
+  }
+  render(){
+    // console.log(Object.keys(data))
+    return <div id="main">
+    <select onClick={this.changeYear}>
+    <option value={null}></option>
+      {Object.keys(data).map((year=>
+      <option key={year} value={year}>{year}</option>
+        ))}
+    </select>
+    <div id='selected-year'>{this.state.selectedYear?`Selected-year-${this.state.selectedYear}`: "No year selected"}</div>
+    <ul>
+      { this.state.selectedYear &&
+        data[this.state.selectedYear].map(movie => {
+          return<li key={movie}>{movie}</li>
+        })
+      }
+    </ul>
     </div>
-  )
+  }
 }
-
 
 export default App;
